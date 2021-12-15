d060e5170d3309cf1ae3cf9a77e7c00a167a7ebd
Christine <christine@christine.nl>
2021-12-15 21:22:48 +0100

    added zaphod.nl as cors domain put version 0.0.3-snapshot

--- src/main/java/nl/christine/websiteserver/controller/PageController.java
+++ src/main/java/nl/christine/websiteserver/controller/PageController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"https://www.zaphod.nl", "https://pengo.christine.nl"}, maxAge = 14400)
+@CrossOrigin(origins = {"https://zaphod.nl", "https://www.zaphod.nl", "https://pengo.christine.nl", "https://christine.nl", "https://www.christine.nl"}, maxAge = 14400)
 public class PageController {
 
     @Autowired
